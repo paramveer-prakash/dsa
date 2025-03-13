3e286b1417501730479dd5e3fc6d7228bb58721a
Paramveer Prakash <paramveer.prakash@gmail.com>
2025-03-14 00:18:21 +0530

    solved - leetcode problem: missing number

---
+++ src/test/java/com/param/lc/array/MissingNumberTest.java
@@ -0,0 +1,14 @@
+package com.param.lc.array;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MissingNumberTest {
+
+    @Test
+    void shouldReturnValidMissingNumber(){
+        MissingNumber missingNumber = new MissingNumber();
+        assertEquals(1,missingNumber.solve(new int[]{0,2}));
+        assertEquals(5,missingNumber.solve(new int[]{0,1,2,3,4,6}));
+    }
+}
