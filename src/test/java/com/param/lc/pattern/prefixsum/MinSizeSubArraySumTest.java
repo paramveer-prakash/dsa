c8c182fdbb6551baa228eb897caa7656038d44e9
Paramveer Prakash <paramveer.prakash@gmail.com>
2025-03-18 22:26:45 +0530

    Solved Leet code: Prefix sum : Minimum Size sub array sum

---
+++ src/test/java/com/param/lc/pattern/prefixsum/MinSizeSubArraySumTest.java
@@ -0,0 +1,13 @@
+package com.param.lc.pattern.prefixsum;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class MinSizeSubArraySumTest {
+
+    @Test
+    void shouldOutPutValidArraySize(){
+        MinSizeSubArraySum minSizeSubArraySum = new MinSizeSubArraySum();
+
+        assertEquals(2, minSizeSubArraySum.solve(new int[]{2,3,1,2,4,3},7));
+    }
+}
