f75d0090b92e9c5ee1f5225ab8986fb3c638e6be
Paramveer Prakash <paramveer.prakash@gmail.com>
2025-03-19 21:44:11 +0530

    Update:Solved Leet code: Prefix sum : Minimum Size sub array sum

--- src/main/java/com/param/lc/pattern/prefixsum/MinSizeSubArraySum.java
+++ src/main/java/com/param/lc/pattern/prefixsum/MinSizeSubArraySum.java
@@ -1,5 +1,7 @@
 package com.param.lc.pattern.prefixsum;
 
+import java.util.*;
+
 public class MinSizeSubArraySum {
 
     public int solve(int[] nums,int target) {
@@ -9,13 +11,10 @@ public class MinSizeSubArraySum {
         if(prefix[0]>=target){
             return 1;
         }
-        for(int i=1;i<nums.length;i++){
-            prefix[i]=prefix[i-1]+nums[i];
-        }
-
         int l=0,r=1,minSize = Integer.MAX_VALUE;
 
         while(r<prefix.length && l<=r){
+            prefix[r]=prefix[r-1]+nums[r];
             int sum = prefix[r]-(l==0?0:prefix[l-1]);
             if(sum>=target){
                 minSize=Math.min(minSize,r-l+1);
