121acf6ebedadba8a7359befe62c04c1f4a48175
Paramveer Prakash <paramveer.prakash@gmail.com>
2025-03-25 16:03:29 +0530

    Update ContainsDuplicate.java

--- src/main/java/com/param/lc/array/ContainsDuplicate.java
+++ src/main/java/com/param/lc/array/ContainsDuplicate.java
@@ -3,6 +3,12 @@ package com.param.lc.array;
 import java.util.HashSet;
 import java.util.Set;
 
+@DSAProblem(
+        name = "Contains Duplicate",
+        number=217,
+        description = "Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.",
+        platform = "LeetCode"
+)
 public class ContainsDuplicate {
 
     public boolean solve(int[] nums){
