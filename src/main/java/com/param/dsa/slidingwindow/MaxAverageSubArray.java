37a857ea298c9e71e060067dbb85455391941b3f
Paramveer Prakash <paramveer.prakash@gmail.com>
2025-03-29 15:50:57 +0530

    Updating annotation

--- src/main/java/com/param/dsa/slidingwindow/MaxAverageSubArray.java
+++ src/main/java/com/param/dsa/slidingwindow/MaxAverageSubArray.java
@@ -1,5 +1,13 @@
 package com.param.dsa.slidingwindow;
 
+import com.param.annotation.DSAProblem;
+
+@DSAProblem(
+        name = "Maximum Average Subarray",
+        number=209,
+        description = "You are given an integer array nums consisting of n elements, and an integer k. Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.",
+        platform = "LeetCode"
+)
 public class MaxAverageSubArray {
 
     public double solve(int[] nums, int k) {
